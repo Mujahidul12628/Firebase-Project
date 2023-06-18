6ba9e6a93e559adaf9fe7c3e511ed837ef87a5a4
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-06-18 23:16:59 +0600

    About functionality

--- first-project/src/assets/Pages/About/About.jsx
+++ first-project/src/assets/Pages/About/About.jsx
@@ -1,10 +1,25 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
+import './About.css'
 
 const About = () => {
+    const [name, setName] = useState('');
+
+    const handleInputChange = (e) => {
+        setName(e.target.value);
+    };
+
     return (
-        <div>
-            <h1>About</h1>
+        <div className="about-container">
+            <h2 className="about-title">About</h2>
+            <p className="about-greeting">Welcome to the About page, <span className="about-name">{name}</span>!</p>
+            <input
+                type="text"
+                value={name}
+                onChange={handleInputChange}
+                placeholder="Enter your name"
+                className="about-input"
+            />
         </div>
     );
 };
